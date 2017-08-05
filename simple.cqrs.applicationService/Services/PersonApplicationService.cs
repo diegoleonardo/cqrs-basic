a21f412f74cf94175ac4ad5478b230c0bb83a1ce
Diego Leonardo <diegoleonardo@outlook.com>
2017-08-05 02:56:31 -0300

    pequena alteração.

--- simple.cqrs.applicationService/Services/PersonApplicationService.cs
+++ simple.cqrs.applicationService/Services/PersonApplicationService.cs
@@ -1,12 +1,10 @@
-﻿using Ninject;
-using simple.cqrs.applicationService.CommandModules.History;
+﻿using simple.cqrs.applicationService.CommandModules.History;
 using simple.cqrs.applicationService.CommandModules.Log;
 using simple.cqrs.applicationService.CommandModules.Person;
 using simple.cqrs.applicationService.DataTransferObjects;
 using simple.cqrs.commands.Implementation.Histories;
 using simple.cqrs.commands.Implementation.Logs;
 using simple.cqrs.commands.Implementation.Persons;
-using simple.cqrs.commands.Interfaces;
 
 namespace simple.cqrs.applicationService.Services
 {
